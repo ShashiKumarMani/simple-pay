2722b99aa1acac7d39254d6f465caef8fb7175bc
Shashi Kumar M <shashikumarm052@gmail.com>
2021-09-23 22:55:20 +0530

    Add components to display error and tx hash

---
+++ src/ErrorMessage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorMessage = ({error}) => {
+    return (
+        <div>
+            <p>{error}</p>
+        </div>
+    )
+}
+
+export default ErrorMessage
---
+++ src/TxList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TxList = ({transaction}) => {
+    return (
+        <div>
+            <p>{transaction}</p>
+        </div>
+    )
+}
+
+export default TxList
